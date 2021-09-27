8e71b7236d333161925650e9412c39ab129e35b4
meniailoA <meniailoishere@gmail.com>
2021-09-27 17:36:10 +0300

    first commit

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,12 @@
 const {Router} = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = Router()
 
-router.get('/', async (req, res) => {
+router.get('/',auth, async (req, res) => {
   const users = await User.find()
   const user = await User.findById(req.session.user._id)
   res.render('index', {
-    title: 'Главная страница',
     isHome: true,
     users,
     user
